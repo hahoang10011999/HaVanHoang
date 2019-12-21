2f860adc81fa8208b39f75c413c0545dfa17169f
Hà Hoàng <hahoang10011999@gmail.com>
2019-12-21 21:10:13 +0700

    1
    1

--- app/src/main/java/com/example/havanhoang_lab5/MainActivity.java
+++ app/src/main/java/com/example/havanhoang_lab5/MainActivity.java
@@ -1,16 +1,33 @@
 package com.example.havanhoang_lab5;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "MainActivity";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getFragment(OrderFragment.newInstance());
+    }
 
+    public void getFragment(Fragment fragment) {
+        try {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG, "getFragment: " + e.getMessage());
+        }
     }
 }
---
+++ app/src/main/java/com/example/havanhoang_lab5/OrderFragment.java
@@ -0,0 +1,122 @@
+package com.example.havanhoang_lab5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.example.havanhoang_lab5.databinding.OrderBinding;
+
+public class OrderFragment extends Fragment {
+    OrderBinding binding;
+    int number = 0;
+    int price = 0;
+    Bundle bundle = new Bundle();
+    public static OrderFragment newInstance() {
+        Bundle args = new Bundle();
+        OrderFragment fragment = new OrderFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater,R.layout.order,container,false);
+        binding.img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(),Your_order.class);
+                startActivity(intent);
+            }
+        });
+        binding.tvPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 String name = "Pizza Panda";
+                 int numberPizza = 0 ;
+                 number = number +1;
+                 numberPizza = numberPizza+1;
+                 price = price + 10;
+                 String pri = Integer.toString(price);
+                String num = Integer.toString(number);
+                binding.tvTotal.setText(pri);
+                binding.tvNumber.setText(num);
+                bundle.putInt("numberPizza",numberPizza);
+                bundle.putString("namePizza",name);
+
+
+            }
+        });
+        binding.tvKFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number +1;
+                price = price + 10;
+                String pri = Integer.toString(price);
+                String num = Integer.toString(number);
+                binding.tvTotal.setText(pri);
+                binding.tvNumber.setText(num);
+            }
+        });
+        binding.tvEgg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number +1;
+                price = price + 10;
+                String pri = Integer.toString(price);
+                String num = Integer.toString(number);
+                binding.tvTotal.setText(pri);
+                binding.tvNumber.setText(num);
+            }
+        });
+        binding.tvCoca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number +1;
+                price = price + 10;
+                String pri = Integer.toString(price);
+                String num = Integer.toString(number);
+                binding.tvTotal.setText(pri);
+                binding.tvNumber.setText(num);
+            }
+        });
+        binding.tvChicken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number +1;
+                price = price + 10;
+                String pri = Integer.toString(price);
+                String num = Integer.toString(number);
+                binding.tvTotal.setText(pri);
+                binding.tvNumber.setText(num);
+            }
+        });
+        binding.tvCup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number = number +1;
+                price = price + 10;
+                String pri = Integer.toString(price);
+                String num = Integer.toString(number);
+                binding.tvTotal.setText(pri);
+                binding.tvNumber.setText(num);
+            }
+        });
+        binding.btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Thank you! Your order is send to restaurant!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        return binding.getRoot();
+    }
+}
